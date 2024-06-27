60a35deb8d879e4f623b5767bd9c8b2867299e02
amandakarolline <amandakarolline@hotmail.com>
2024-06-27 03:06:41 -0300

    Atualização API discente

--- src/api/discente/routes/discente.js
+++ src/api/discente/routes/discente.js
@@ -7,3 +7,25 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::discente.discente');
+
+module.exports = {
+    routes: [
+        {
+            "method": "GET",
+            "path": "/discentes",
+            "handler": "discente.findAll",
+            "config": {
+                "policies": [],
+            }
+        },
+        {
+            "method": "GET",
+            "path": "/discentes/:attribute/:value",
+            "handler": "discente.findByAttribute",
+            "config": {
+                "policies": [],
+            }
+        },
+        
+    ]
+}
