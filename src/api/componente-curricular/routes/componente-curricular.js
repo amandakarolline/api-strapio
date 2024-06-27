ed5780cefc45ba592d56787b1f38f8280e92b3f9
amandakarolline <amandakarolline@hotmail.com>
2024-06-27 02:09:28 -0300

    Atualização API componente curricular

--- src/api/componente-curricular/routes/componente-curricular.js
+++ src/api/componente-curricular/routes/componente-curricular.js
@@ -7,3 +7,25 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::componente-curricular.componente-curricular');
+
+module.exports = {
+    routes: [
+        {
+            "method": "GET",
+            "path": "/componentes-curriculares",
+            "handler": "componente-curricular.findAll",
+            "config": {
+                "policies": [],
+            }
+        },
+        {
+            "method": "GET",
+            "path": "/componentes-curriculares/:attribute/:value",
+            "handler": "componente-curricular.findByAttribute",
+            "config": {
+                "policies": [],
+            }
+        },
+        
+    ]
+}
