a64adf2045b858ea48d568f1480707bf67e1d5b9
amandakarolline <amandakarolline@hotmail.com>
2024-06-27 02:25:35 -0300

    Alteração API grade componente

--- src/api/grade-componente/controllers/grade-componente.js
+++ src/api/grade-componente/controllers/grade-componente.js
@@ -5,5 +5,32 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const { sanitize } = require('@strapi/utils');
+const { contentAPI } = sanitize;
 
 module.exports = createCoreController('api::grade-componente.grade-componente');
+
+module.exports = {
+    async findByAttribute(ctx) {
+        const { attribute, value } = ctx.params;
+        const model = strapi.getModel('api::grade-componente.grade-componente');
+
+        const entity = await strapi.db.query('api::grade-componente.grade-componente').findOne({ where: { [attribute]: value } });
+
+        if (!entity) {
+            return ctx.notFound('Entity not found');
+        }
+
+        const sanitizedEntity = await contentAPI.output(entity, model);
+        return ctx.send(sanitizedEntity);
+    },
+
+    async findAll(ctx) {
+        const model = strapi.getModel('api::grade-componente.grade-componente');
+        const entities = await strapi.db.query('api::grade-componente.grade-componente').findMany();
+
+        const sanitizedEntities = await contentAPI.output(entities, model);
+
+        return ctx.send(sanitizedEntities);
+    },
+};
--- src/api/grade-componente/routes/grade-componente.js
+++ src/api/grade-componente/routes/grade-componente.js
@@ -7,3 +7,25 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::grade-componente.grade-componente');
+
+module.exports = {
+    routes: [
+        {
+            "method": "GET",
+            "path": "/grade-componentes",
+            "handler": "grade-componente.findAll",
+            "config": {
+                "policies": [],
+            }
+        },
+        {
+            "method": "GET",
+            "path": "/grade-componentes/:attribute/:value",
+            "handler": "grade-componente.findByAttribute",
+            "config": {
+                "policies": [],
+            }
+        },
+        
+    ]
+}
