77747a610b570ef64bc4755c403d6b0d140bb455
amandakarolline <amandakarolline@hotmail.com>
2024-06-27 02:40:41 -0300

    Atualização API grade curricular

--- src/api/grade-curricular/controllers/grade-curricular.js
+++ src/api/grade-curricular/controllers/grade-curricular.js
@@ -5,5 +5,32 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const { sanitize } = require('@strapi/utils');
+const { contentAPI } = sanitize;
 
 module.exports = createCoreController('api::grade-curricular.grade-curricular');
+
+module.exports = {
+    async findByAttribute(ctx) {
+        const { attribute, value } = ctx.params;
+        const model = strapi.getModel('api::grade-curricular.grade-curricular');
+      
+        const entity = await strapi.db.query('api::grade-curricular.grade-curricular').findOne({ where: { [attribute]: value } });
+      
+        if (!entity) {
+            return ctx.notFound('Entity not found');
+        }
+  
+        const sanitizedEntity = await contentAPI.output(entity, model);
+        return ctx.send(sanitizedEntity);
+    },
+  
+    async findAll(ctx) {
+        const model = strapi.getModel('api::grade-curricular.grade-curricular');
+        const entities = await strapi.db.query('api::grade-curricular.grade-curricular').findMany();
+  
+        const sanitizedEntities = await contentAPI.output(entities, model);
+  
+        return ctx.send(sanitizedEntities);
+    },
+};
--- src/api/grade-curricular/routes/grade-curricular.js
+++ src/api/grade-curricular/routes/grade-curricular.js
@@ -7,3 +7,25 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::grade-curricular.grade-curricular');
+
+module.exports = {
+    routes: [
+        {
+            "method": "GET",
+            "path": "/grade-curriculares",
+            "handler": "grade-curricular.findAll",
+            "config": {
+                "policies": [],
+            }
+        },
+        {
+            "method": "GET",
+            "path": "/grade-curriculares/:attribute/:value",
+            "handler": "grade-curricular.findByAttribute",
+            "config": {
+                "policies": [],
+            }
+        },
+        
+    ]
+}
