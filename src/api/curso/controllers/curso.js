064306d23ff5b46518dbfe752de03a3eb931c24b
amandakarolline <amandakarolline@hotmail.com>
2024-06-27 02:18:38 -0300

    Atualização API curso

--- src/api/curso/controllers/curso.js
+++ src/api/curso/controllers/curso.js
@@ -5,5 +5,32 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const { sanitize } = require('@strapi/utils');
+const { contentAPI } = sanitize;
 
 module.exports = createCoreController('api::curso.curso');
+
+module.exports = {
+    async findByAttribute(ctx) {
+        const { attribute, value } = ctx.params;
+        const model = strapi.getModel('api::curso.curso');
+      
+        const entity = await strapi.db.query('api::curso.curso').findOne({ where: { [attribute]: value } });
+      
+        if (!entity) {
+            return ctx.notFound('Entity not found');
+        }
+  
+        const sanitizedEntity = await contentAPI.output(entity, model);
+        return ctx.send(sanitizedEntity);
+    },
+  
+    async findAll(ctx) {
+        const model = strapi.getModel('api::curso.curso');
+        const entities = await strapi.db.query('api::curso.curso').findMany();
+  
+        const sanitizedEntities = await contentAPI.output(entities, model);
+  
+        return ctx.send(sanitizedEntities);
+    },
+  };
--- src/api/curso/routes/curso.js
+++ src/api/curso/routes/curso.js
@@ -7,3 +7,25 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::curso.curso');
+
+module.exports = {
+    routes: [
+        {
+            "method": "GET",
+            "path": "/cursos",
+            "handler": "curso.findAll",
+            "config": {
+                "policies": [],
+            }
+        },
+        {
+            "method": "GET",
+            "path": "/cursos/:attribute/:value",
+            "handler": "curso.findByAttribute",
+            "config": {
+                "policies": [],
+            }
+        },
+        
+    ]
+}
