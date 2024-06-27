db97031dcc99109757d90f5514aef7a55ebaa2f7
amandakarolline <amandakarolline@hotmail.com>
2024-06-26 22:51:05 -0300

    Atualização da API docentes

--- src/api/docente/routes/docente.js
+++ src/api/docente/routes/docente.js
@@ -7,3 +7,25 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::docente.docente');
+
+module.exports = {
+    routes: [
+        {
+            "method": "GET",
+            "path": "/docentes",
+            "handler": "docente.findAll",
+            "config": {
+                "policies": [],
+            }
+        },
+        {
+            "method": "GET",
+            "path": "/docentes/:attribute/:value",
+            "handler": "docente.findByAttribute",
+            "config": {
+                "policies": [],
+            }
+        },
+        
+    ]
+}
