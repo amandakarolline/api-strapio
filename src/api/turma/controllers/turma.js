b698ccdbd031ffa250fc7194f973094627e9ab00
amandakarolline <amandakarolline@hotmail.com>
2024-06-27 03:04:24 -0300

    Atualização API turmas

--- src/api/turma/controllers/turma.js
+++ src/api/turma/controllers/turma.js
@@ -5,5 +5,32 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const { sanitize } = require('@strapi/utils');
+const { contentAPI } = sanitize;
 
 module.exports = createCoreController('api::turma.turma');
+
+module.exports = {
+    async findByAttribute(ctx) {
+      const { attribute, value } = ctx.params;
+      const model = strapi.getModel('api::turma.turma');
+      
+      const entity = await strapi.db.query('api::turma.turma').findMany({ where: { [attribute]: value } });
+      
+      if (!entity) {
+        return ctx.notFound('Entity not found');
+      }
+  
+      const sanitizedEntity = await contentAPI.output(entity, model);
+      return ctx.send(sanitizedEntity);
+    },
+  
+    async findAll(ctx) {
+      const model = strapi.getModel('api::turma.turma');
+      const entities = await strapi.db.query('api::turma.turma').findMany();
+  
+      const sanitizedEntities = await contentAPI.output(entities, model);
+  
+      return ctx.send(sanitizedEntities);
+    },
+  };
--- src/api/turma/routes/turma.js
+++ src/api/turma/routes/turma.js
@@ -7,3 +7,25 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::turma.turma');
+
+module.exports = {
+    routes: [
+        {
+            "method": "GET",
+            "path": "/turmas",
+            "handler": "turma.findAll",
+            "config": {
+                "policies": [],
+            }
+        },
+        {
+            "method": "GET",
+            "path": "/turmas/:attribute/:value",
+            "handler": "turma.findByAttribute",
+            "config": {
+                "policies": [],
+            }
+        },
+        
+    ]
+}
