56b991e0b4bb25375885156233f5599ce513116a
amandakarolline <amandakarolline@hotmail.com>
2024-06-27 02:51:17 -0300

    Alteração API matricula

--- src/api/matricula/routes/matricula.js
+++ src/api/matricula/routes/matricula.js
@@ -7,3 +7,25 @@
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::matricula.matricula');
+
+module.exports = {
+    routes: [
+        {
+            "method": "GET",
+            "path": "/matriculas",
+            "handler": "matricula.findAll",
+            "config": {
+                "policies": [],
+            }
+        },
+        {
+            "method": "GET",
+            "path": "/matriculas/:attribute/:value",
+            "handler": "matricula.findByAttribute",
+            "config": {
+                "policies": [],
+            }
+        },
+        
+    ]
+}
